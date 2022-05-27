ee9f0afcfa52fe34d2b1f69d5b49657603b9ec0c
Suellen Lima <sulima19977991@gmail.com>
2022-05-27 16:54:08 -0300

    Implementando tratamento de exceções de status

--- src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -23,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algalog.domain.model.Cliente;
 import com.algaworks.algalog.domain.repository.ClienteRepository;
+import com.algaworks.algalog.domain.service.CatalogoClienteService;
 
 import lombok.AllArgsConstructor;
 
@@ -33,6 +34,8 @@ public class ClienteController {
 	
 	@Autowired
 	private ClienteRepository clienteRepository;
+	@Autowired
+	private CatalogoClienteService catalogoClienteService;
 	
 	@GetMapping
 	public List<Cliente> listar() {
@@ -52,7 +55,7 @@ public class ClienteController {
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public Cliente adicionar(@Valid @RequestBody Cliente cliente) {
-		return clienteRepository.save(cliente);
+		return catalogoClienteService.salvar(cliente);
 	}
 	
 	@PutMapping("/{clienteId}")
@@ -63,7 +66,7 @@ public class ClienteController {
 		}
 		
 		cliente.setId(clienteId);
-		cliente = clienteRepository.save(cliente);
+		cliente = catalogoClienteService.salvar(cliente);
 		
 		return ResponseEntity.ok(cliente);
 		
@@ -76,7 +79,7 @@ public class ClienteController {
 	if(!clienteRepository.existsById(clienteId)){
 		return ResponseEntity.notFound().build();
 	}
-	clienteRepository.deleteById(clienteId);
+	catalogoClienteService.excluir(clienteId);
 	
 	return ResponseEntity.noContent().build();
 		
--- src/main/java/com/algaworks/algalog/domain/repository/ClienteRepository.java
+++ src/main/java/com/algaworks/algalog/domain/repository/ClienteRepository.java
@@ -1,6 +1,8 @@
 package com.algaworks.algalog.domain.repository;
 
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,6 @@ import com.algaworks.algalog.domain.model.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+	
+	Optional<Cliente> findByEmail(String email);
 }
